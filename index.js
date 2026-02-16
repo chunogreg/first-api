169dd24d27ce0571785671ea4eb538816265fbb0
chunogreg <chunogreg@yahoo.com>
2026-02-16 10:54:56 +0100

    Add task api with Get and Post route

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+let tasks = [{id : 1, title: "learn Nodejs", done:false}, {id : 2, title: "Build first API", done:false} ];
+
 app.get("/", (req, res) => {
-  res.send("My first API is running! THIS IS CHUNOGREG ....");
+  res.send("Task API is running");
+});
+
+app.get("/api/tasks", (req, res)=>{ 
+  res.json(tasks);
+})
+
+app.post("/api/tasks", (req, res)=>{
+  const newTask = { id: tasks.length+1, title: req.body.title , done: false};
+  
+tasks.push(newTask);
+res.json(newTask);
 });
 
 const PORT = 3000;

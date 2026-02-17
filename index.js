c08b2b0c25927d7d50caf30a8bbdd08943ae7d85
chunogreg <chunogreg@yahoo.com>
2026-02-17 20:26:00 +0100

    Add logging, environmental config and error handling

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
+require("dotenv").config;
 const express = require("express");
-const app = express();
+const morgan = require("morgan");
 
+const app = express();
 const taskRoutes = require("./routes/tasks");
 
 app.use(express.json());
+app.use(morgan("dev"));
+
 app.get("/", (req, res) => {
   res.send("Task API is running");
 });
 
 app.use("/api/tasks", taskRoutes);
 
+//unknown endpoint handler
+app.use((req, res) => {
+  res.status(404).json({ error: "unknown endpoint" });
+});
 
-//let tasks = [{id : 1, title: "learn Nodejs", done:false}, {id : 2, title: "Build first API", done:false} ];
-
-//app.get("/api/tasks", (req, res)=>{ 
- // res.end(JSON.stringify(tasks, null, 2));
-//})
-
-//app.post("/api/tasks", (req, res)=>{
-  //const newTask = { id: tasks.length+1, title: req.body.title , done: false};
-  
-//tasks.push(newTask);
-//res.json(newTask);
-//});
-
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
